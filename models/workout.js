7ea11df643e01718a168daaf9b9a8382f5955346
kkinge79 <kingerykagan@gmail.com>
2022-01-26 15:41:20 -0600

    workoutmodel temp may fix later

---
+++ models/workout.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const workoutSchema = new Schema ({
+  name: String,
+  date: date,
+  time: Number,
+  bodyweight: Number,
+  focus: String,
+  regime: [String],
+  owner: {type: Schema.Types.ObjectId, 'ref': "Profile"}
+})
+
+const Workout = mongoose.model('Workout', workoutSchema)
+
+export {
+  Workout
+}
